146ac9c775a3728d4567dd9fc805ab0c132587d8
sahaymihir <mihir.sahay0123@hotmail.com>
2023-08-05 18:58:46 +0530

    Area Of Circle

---
+++ Area Of Circle.c
@@ -0,0 +1,9 @@
+# include<stdio.h>
+
+int main () {
+    float radius;
+    printf("Enter Radius of Circle \n");
+    scanf("%f", &radius);
+    printf("Area Of Circle is %f", 3.14 * radius * radius);
+    return 0;
+}
