a74b997918e9b4e76bf83dc0b96eee3e8d689d85
sahaymihir <mihir.sahay0123@hotmail.com>
2023-08-06 13:17:49 +0530

    Modify to find the circumference too

--- Area Of Circle.c
+++ Area Of Circle.c
@@ -4,6 +4,7 @@ int main () {
     float radius;
     printf("Enter Radius of Circle \n");
     scanf("%f", &radius);
-    printf("Area Of Circle is %f", 3.14 * radius * radius);
+    printf("The Area Of Circle is %f \n", 3.14 * radius * radius);
+    printf("The Circumference of Circle is %f  \n", 2*3.14*radius);
     return 0;
 }
