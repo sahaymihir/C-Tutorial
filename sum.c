ac6c8060929800812196fad011726b7ed80390cd
sahaymihir <mihir.sahay0123@hotmail.com>
2024-01-07 23:55:16 +0530

    Sum

---
+++ sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+
+int main () {
+    int a,b;
+    printf("Input two numbers \n");
+    scanf("%d %d",&a,&b);
+    printf("The sum is %d \n",a+b);
+    return 0;
+}
