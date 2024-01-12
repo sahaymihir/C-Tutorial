1be77da7862eb1ea0ceba5a2b42b866fb4e85ea6
sahaymihir <mihir.sahay0123@hotmail.com>
2024-01-12 18:48:57 +0530

    Make marks as a function

---
+++ functions/marks.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+void marks();
+
+int main() {
+    marks();
+    return 0;
+}
+
+// defined marks function
+
+void marks () {
+    float marks;
+    printf("Enter your marks :");
+    scanf("%f", &marks);
+    if (marks > 90 && marks <= 100) {
+        printf("You got A+ grade \n");
+    }
+    else if (marks > 80 && marks <= 90) {
+        printf("You got A grade \n");
+    }
+    else if (marks > 70 && marks <= 80) {
+        printf("You got B grade \n");
+    }
+    else if (marks > 60 && marks <= 70) {
+        printf("You got C grade \n");
+    }
+    else if (marks > 50 && marks <= 60) {
+        printf("You got D grade \n");
+    }
+    else if (marks > 40 && marks <= 50) {
+        printf("You got E grade \n");
+    }
+    else if (marks >= 0 && marks <= 40) {
+        printf("You got F grade \n");
+    }
+    else {
+        printf("Please enter valid marks \n");
+    }
+}
